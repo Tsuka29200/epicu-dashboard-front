961e43182f374af29d7fb851030f0bbeb40beea8
Tsuka29200 <168766293+Tsuka29200@users.noreply.github.com>
2024-05-03 01:51:45 +0530

    changed backend url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Login from "./components/Auth/Login";
 import AppRouter from "./AppRouter";
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://app-b273150c-9f46-40f7-8dc3-1e4d9a5d318b.cleverapps.io/";
 const App = () => {
   const [user, setUser] = useState({ auth: false, name: "" });
   return (
@@ -10,7 +10,7 @@ const App = () => {
       {user.auth ? (
         <AppRouter user={user} setUser={setUser} />
       ) : (
-        <Login setUser={setUser} />
+        <Login setUser={setUser} /> 
       )}
     </div>
   );
